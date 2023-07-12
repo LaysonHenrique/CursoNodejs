3fb922138522ab0b40f59a256ed4a9740228816f
LaysonHenrique <dasilvalayson@gmail.com>
2023-07-12 09:45:46 -0400

    new file:   EstruturaDeProgramacao/3-exercicios e renamed files:   Exercicios -> ExerciciosIntroducao

---
+++ EstruturaDeProgramacao/3-exercicios/script.js
@@ -0,0 +1,56 @@
+//EXERCICIOS SOBRE ESTRUTURA DE PROGRAMAÇÃO, ONDE SERA ABORDADA ESTRTURA DE CONTROLE E DE REPETIÇÃO.
+/*
+*****************************************************************************************************************************
+EXERCICIO 1
+armazene valores em variaveis dos tipos number, boolean e string e imprima no console o tipo de cada variavel.
+let a = 1;
+let b = true;
+let c = "teste";
+console.log(typeof a, typeof b, typeof c);
+Obs:typeof é um operador que nos retorna o tipo do dado de uma variavel.
+
+*****************************************************************************************************************************
+EXERCICIO 2
+crie uma estrutura condicional usando o if para controlar a entrada de uma balada, onde se a pessoa tiver idade maior ou igual a 18 anos ela pode entrar, caso contrario 
+ela não pode entrar.
+
+let idade = prompt("Digite sua idade: ");
+
+if(idade > 18){
+    console.log("Pode entrar");
+}else{
+    console.log("Não pode entrar");
+}
+
+*****************************************************************************************************************************
+EXERCICIO 3
+
+*****************************************************************************************************************************
+EXERCICIO 4
+
+*****************************************************************************************************************************
+EXERCICIO 5
+
+*****************************************************************************************************************************
+EXERCICIO 6
+
+*****************************************************************************************************************************
+EXERCICIO 7
+
+*****************************************************************************************************************************
+EXERCICIO 8
+
+*****************************************************************************************************************************
+EXERCICIO 9
+
+*****************************************************************************************************************************
+EXERCICIO 10
+
+*/
+let idade = prompt("Digite sua idade: ");
+
+if(idade > 18){
+    console.log("Pode entrar");
+}else{
+    console.log("Não pode entrar");
+}
