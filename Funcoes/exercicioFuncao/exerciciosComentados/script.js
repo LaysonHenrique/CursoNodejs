040363c26889b45bf2639ca3a852ca16cada8a88
LaysonHenrique <dasilvalayson@gmail.com>
2023-07-16 23:16:53 -0400

    New file "exerciciosExtras"

--- Funcoes/exercicioFuncao/exerciciosComentados/script.js
+++ Funcoes/exercicioFuncao/exerciciosComentados/script.js
@@ -114,3 +114,21 @@ decrementaPares(10);
 ****************************************************************************************************
 
 */
+function faixaEtaria(idade){
+    if(idade < 12){
+        idade="Criança";
+        return idade;
+        
+    }else if(idade >= 12 && idade <= 24){
+        idade = "Jovem";
+        return idade;
+    }else if(idade => 25){
+        idade = "Adulto";
+        return idade;
+    }
+}
+
+let crianca = faixaEtaria(10);
+console.log(crianca);
+let adulto = faixaEtaria(24);
+console.log(adulto);    
